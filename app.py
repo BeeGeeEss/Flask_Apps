4889dcf28a70095aa60d7cdba01ffb1636d67199
BeeGeeEss <brando.g.smith@gmail.com>
2025-10-01 22:12:27 +1000

    Functional Flask Apps

--- app.py
+++ app.py
@@ -5,3 +5,21 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/goodbye/")
+def goodbye_world():
+    return "<p>Goodbye, World!</p>"
+
+@app.route("/coder/")
+def coder():
+    return "<p>This web app was created in a class at Coder Academy.</p>"
+
+# from flask import Flask
+# from datetime import datetime
+
+# app = Flask(__name__)
+
+# # Your code here
+# @app.route("/current_time/")
+# def current_time():
+#     return f"<p>{str(datetime.now().strftime('%H:%M'))}</p>"
